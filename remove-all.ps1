f1632a4f54679c9b2850a33d9c6353806d76ed15
TB <q0015300153@gmail.com>
2021-06-07 17:33:16 +0800

    update for windows

--- remove-all.ps1
+++ remove-all.ps1
@@ -1,4 +1,3 @@
-#!/bin/bash
 ###############################
 # 移除所有 docker 資源         #
 # 警告!謹慎使用!               #
@@ -6,8 +5,8 @@
 # Warning! Use with caution!  #
 ###############################
 
-docker stop `docker ps -qa`
-docker rm `docker ps -qa`
-docker rmi -f `docker images -qa`
-docker volume rm `docker volume ls -q`
-docker network rm `docker network ls -q`
+docker stop $(docker ps -qa)
+docker rm $(docker ps -qa)
+docker rmi -f $(docker images -qa)
+docker volume rm $(docker volume ls -q)
+docker network rm $(docker network ls -q)
