afb76642822d1e0541991d32b083226e59ee9588
TB <q0015300153@gmail.com>
2021-06-07 16:34:56 +0800

    update for windows

--- docker-stop.ps1
+++ docker-stop.ps1
@@ -1,4 +1,3 @@
-#!/bin/bash
 ################################################################
 # 用來停止指定資料夾的 docker-compose 服務                       #
 # The docker-compose service used to stop the specified folder #
@@ -6,29 +5,29 @@
 
 # tw: 設定要停止的服務
 # en: Set the service to start
-. $(dirname "$0")/projects-list.sh
+Import-module "$PSScriptRoot\projects-list.ps1" -Force
 
 # tw: 整理要停止的專案
 # en: Organize the project to stop
-list=()
-for name in "${!projects[@]}"; do
-	list+=(${name})
-done
+$list = @()
+foreach ($name in $projects.keys) {
+    $list+= $name
+}
 
 # tw: 取得要停止的服務設定檔 docker-compose.yml
 # en: Get the service profile to stop docker-compose.yml
-ymls=()
-for i in "${list[@]}"; do
-	ymls+=(`find ./${i} -type f -name 'docker-compose.yml'`)
-done
+$ymls = @()
+foreach ($i in $list) {
+    $ymls+= (Get-Childitem –Path "$i/docker-compose.yml").fullname
+}
 
 # tw: 執行 docker-compose stop
 # en: Execute docker-compose stop
-thisPath=$(cd `dirname $0`; pwd)
-if [ $# -eq 0 ]; then
-    for yml in "${ymls[@]}"; do
-    	cd `dirname "${yml}"`
-    	docker-compose -f `basename "${yml}"` stop
-    	cd ${thisPath}
-    done
-fi
+$thisPath = $PWD
+if ($args.Count -eq 0) {
+    foreach ($yml in $ymls) {
+        cd (Split-Path -Path $yml)
+        docker-compose -f (Split-Path -Path $yml -Leaf) stop
+        cd $thisPath
+    }
+}
